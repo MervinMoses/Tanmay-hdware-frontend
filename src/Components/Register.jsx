edebe3d134de7f912ee78847ae4fd022dc761c10
MervinMoses <princemervin30@gmail.com>
2023-09-25 22:36:00 +0530

    reg_log_linked

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { database } from '../FirebaseConfig';
+import { NavLink } from 'react-router-dom';
 
 export const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(database, email, password)
+    .then((userCredential) =>{
+      console.log(userCredential);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
   return (
 <>
 <div class="wrapper d-flex flex-column min-vh-100 bg-light">
@@ -14,10 +29,8 @@ export const Register = () => {
       <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
         <div class="d-flex justify-content-center py-4">
-          <a href="index.html" class="logo d-flex align-items-center w-auto">
             <img src="assets/img/logo.png" alt=""/>
-            <span class="d-none d-lg-block">NiceAdmin</span>
-          </a>
+            <span class="d-none d-lg-block">Tanmay Hardware</span>
         </div>
 
         <div class="card mb-3">
@@ -26,49 +39,32 @@ export const Register = () => {
 
             <div class="pt-4 pb-2">
               <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
-              <p class="text-center small">Enter your personal details to create account</p>
             </div>
 
-            <form class="row g-3 needs-validation" novalidate>
+            <form class="row g-3 needs-validation"  onSubmit={register} novalidate>
               <div class="col-12">
-                <label for="yourName" class="form-label">Your Name</label>
+                <label for="yourName" class="form-label">Name</label>
                 <input type="text" name="name" class="form-control" id="yourName" required/>
                 <div class="invalid-feedback">Please, enter your name!</div>
               </div>
 
               <div class="col-12">
-                <label for="yourEmail" class="form-label">Your Email</label>
-                <input type="email" name="email" class="form-control" id="yourEmail" required/>
+                <label for="yourEmail" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="yourEmail" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 <div class="invalid-feedback">Please enter a valid Email adddress!</div>
               </div>
 
               <div class="col-12">
-                <label for="yourUsername" class="form-label">Username</label>
-                <div class="input-group has-validation">
-                  <span class="input-group-text" id="inputGroupPrepend">@</span>
-                  <input type="text" name="username" class="form-control" id="yourUsername" required/>
-                  <div class="invalid-feedback">Please choose a username.</div>
-                </div>
-              </div>
-
-              <div class="col-12">
                 <label for="yourPassword" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control" id="yourPassword" required/>
+                <input type="password" name="password" class="form-control" id="yourPassword" value={password} onChange={(e) => setPassword(e.target.value)} required/>
                 <div class="invalid-feedback">Please enter your password!</div>
               </div>
 
               <div class="col-12">
-                <div class="form-check">
-                  <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required/>
-                  <label class="form-check-label" for="acceptTerms">I agree and accept the <a href="#">terms and conditions</a></label>
-                  <div class="invalid-feedback">You must agree before submitting.</div>
-                </div>
-              </div>
-              <div class="col-12">
                 <button class="btn btn-primary w-100" type="submit">Create Account</button>
               </div>
               <div class="col-12">
-                <p class="small mb-0">Already have an account? <a href="pages-login.html">Log in</a></p>
+                <p class="small mb-0">Already have an account? <NavLink to ="Login">Log in</NavLink></p>
               </div>
             </form>
 
