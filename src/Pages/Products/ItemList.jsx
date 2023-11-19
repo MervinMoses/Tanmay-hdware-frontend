46e7b7a16e2ccb7fd461ca443f4bf8ee4a8193b7
JASSIM D <110032954+jaassim@users.noreply.github.com>
2023-11-20 00:36:06 +0530

    testing
    test commit

--- src/Pages/Products/ItemList.jsx
+++ src/Pages/Products/ItemList.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import PageHeader from "../../components/PageHeader";
+import PageHeader from "../../Components/PageHeader";
 import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
 import { Paper, makeStyles, TableBody, TableRow, TableCell, Toolbar, InputAdornment } from '@material-ui/core';
-import useTable from "../../components/useTable";
+import useTable from "../../Components/useTable";
 import * as CustomerService from "../../Services/CustomerService"
-import Controls from "../../components/controls/Controls";
+import Controls from "../../Components/controls/Controls";
 import { Search } from "@material-ui/icons";
 import AddIcon from '@material-ui/icons/Add';
-import Popup from "../../components/Popup";
+import Popup from "../../Components/Popup";
 import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 import CloseIcon from '@material-ui/icons/Close';
 import ProductG from './ProductG';
